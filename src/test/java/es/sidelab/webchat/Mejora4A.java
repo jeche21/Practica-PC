fbd75fa2334b91bc4d04ea5dac660b042a2cef37
jeche21 <jesus-alcorcon@hotmail.com>
2017-06-08 00:39:50 +0200

    Mejora4

---
+++ src/test/java/es/sidelab/webchat/Mejora4A.java
@@ -0,0 +1,111 @@
+package es.sidelab.webchat;
+
+import java.util.ConcurrentModificationException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.junit.Test;
+
+import es.codeurjc.webchat.Chat;
+import es.codeurjc.webchat.ChatManager;
+
+public class Mejora4A {
+	static CountDownLatch latchUser = new CountDownLatch(3);
+	static Semaphore semaforo = new Semaphore (0);
+
+	static ChatManager chatManager = new ChatManager(5);
+	static Chat chat;
+	
+	ExecutorService executor = Executors.newSingleThreadExecutor();
+	CompletionService<String> completionService = new ExecutorCompletionService<String>(executor);
+	
+	ExecutorService executor2 = Executors.newSingleThreadExecutor();
+	CompletionService<String> completionService2 = new ExecutorCompletionService<String>(executor2);
+	
+	ExecutorService executor3 = Executors.newSingleThreadExecutor();
+	CompletionService<String> completionService3 = new ExecutorCompletionService<String>(executor3);
+	
+	ExecutorService executor4 = Executors.newSingleThreadExecutor();
+	CompletionService<String> completionService4 = new ExecutorCompletionService<String>(executor4);
+	
+	@Test
+	public void testMejora4() throws Throwable {
+		
+		Callable <String> tarea = ()-> {
+			crearChat("Jesus");
+			Thread.sleep(1000);
+			return "Jesus";
+		};
+		completionService.submit(tarea);
+		
+		Callable <String> tareaUser1 = ()-> {
+			restoUsuarios("César");
+			Thread.sleep(1000);
+			return "Cesar";
+		};
+		completionService2.submit(tareaUser1);
+		
+		Callable <String> tareaUser2 = ()-> {
+			restoUsuarios("Javi");
+			Thread.sleep(1000);
+			return "Javi";
+		};
+		completionService3.submit(tareaUser2);
+		
+		Callable <String> tareaUser3 = ()-> {
+			restoUsuarios("Miguel");
+			Thread.sleep(1000);
+			return "Miguel";
+		};
+		completionService4.submit(tareaUser3);
+		
+		
+		try{
+			Future<String> f = completionService.take();
+			System.out.println("Terminado " + f.get());
+			
+			Future<String> f2 = completionService2.take();
+			System.out.println("Terminado " + f2.get());
+			
+			Future<String> f3 = completionService3.take();
+			System.out.println("Terminado " + f3.get());
+			
+			Future<String> f4 = completionService4.take();
+			System.out.println("Terminado " + f4.get());
+		} catch (ExecutionException | ConcurrentModificationException e) {
+			throw e.getCause();
+		}
+	}
+
+	public void crearChat(String nombre) throws InterruptedException,TimeoutException {
+
+		TestUser user = new TestUser(nombre);
+		chatManager.newUser(user);
+		chat = chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
+		chat.addUser(user);
+		semaforo.release();
+		latchUser.await();		
+		
+		chat.sendMessage(user, "Hola,¿Que tal estais?");		
+	}
+	
+	public void restoUsuarios(String usuario) throws InterruptedException,TimeoutException {
+		semaforo.acquire();
+		TestUser user = new TestUser(usuario);
+		chatManager.newUser(user);
+		chat.addUser(user);
+
+		latchUser.countDown();
+
+	}
+}
+
--- src/test/java/es/sidelab/webchat/Mejora4B.java
+++ src/test/java/es/sidelab/webchat/Mejora4B.java
@@ -1,27 +1,36 @@
 package es.sidelab.webchat;
 
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import es.codeurjc.webchat.Chat;
 import es.codeurjc.webchat.ChatManager;
+import es.codeurjc.webchat.User;
 
-public class Mejora4 {
-	static CountDownLatch latchUser = new CountDownLatch(3);
-	static Semaphore semaforo = new Semaphore (0);
+public class Mejora4B {
+	String [] mensajesOrdenados = {"Hola", "¿Que tal?", "Bien", "Me alegro", "Adios"};
+	List <String> mensajes = new ArrayList<String>();
+	Exchanger<Boolean> intercambiador= new Exchanger<Boolean>();
+	
 
+	CountDownLatch finChat = new CountDownLatch(1);
+	CountDownLatch nuevoUsuario = new CountDownLatch(1);
 	static ChatManager chatManager = new ChatManager(5);
 	static Chat chat;
 	
@@ -31,43 +40,22 @@ public class Mejora4 {
 	ExecutorService executor2 = Executors.newSingleThreadExecutor();
 	CompletionService<String> completionService2 = new ExecutorCompletionService<String>(executor2);
 	
-	ExecutorService executor3 = Executors.newSingleThreadExecutor();
-	CompletionService<String> completionService3 = new ExecutorCompletionService<String>(executor3);
-	
-	ExecutorService executor4 = Executors.newSingleThreadExecutor();
-	CompletionService<String> completionService4 = new ExecutorCompletionService<String>(executor4);
-	
 	@Test
 	public void testMejora4() throws Throwable {
 		
 		Callable <String> tarea = ()-> {
 			crearChat("Jesus");
 			Thread.sleep(1000);
-			return null;
+			return "Jesus";
 		};
 		completionService.submit(tarea);
 		
 		Callable <String> tareaUser1 = ()-> {
 			restoUsuarios("César");
 			Thread.sleep(1000);
-			return null;
+			return "Cesar";
 		};
 		completionService2.submit(tareaUser1);
-		
-		Callable <String> tareaUser2 = ()-> {
-			restoUsuarios("Javi");
-			Thread.sleep(1000);
-			return null;
-		};
-		completionService3.submit(tareaUser2);
-		
-		Callable <String> tareaUser3 = ()-> {
-			restoUsuarios("Miguel");
-			Thread.sleep(1000);
-			return null;
-		};
-		completionService4.submit(tareaUser3);
-		
 		
 		try{
 			Future<String> f = completionService.take();
@@ -75,37 +63,46 @@ public class Mejora4 {
 			
 			Future<String> f2 = completionService2.take();
 			System.out.println("Terminado " + f2.get());
-			
-			Future<String> f3 = completionService3.take();
-			System.out.println("Terminado " + f3.get());
-			
-			Future<String> f4 = completionService4.take();
-			System.out.println("Terminado " + f4.get());
 		} catch (ExecutionException | ConcurrentModificationException e) {
 			throw e.getCause();
 		}
 	}
 
 	public void crearChat(String nombre) throws InterruptedException,TimeoutException {
-
 		TestUser user = new TestUser(nombre);
 		chatManager.newUser(user);
 		chat = chatManager.newChat("Chat", 5, TimeUnit.SECONDS);
-		chat.addUser(user);
-		semaforo.release();
-		latchUser.await();		
-		
-		chat.sendMessage(user, "Hola,¿Que tal estais?");		
+		chat.addUser(user);	
+		finChat.countDown();
+		nuevoUsuario.await();
+		for(int i = 0; i < 5; i++){
+			chat.sendMessage(user, mensajesOrdenados[i]);
+		}
+		boolean iguales = intercambiador.exchange(null);
+		assertTrue(iguales);
 	}
 	
 	public void restoUsuarios(String usuario) throws InterruptedException,TimeoutException {
-		semaforo.acquire();
-		TestUser user = new TestUser(usuario);
+		finChat.await();
+		TestUser user = new TestUser(usuario){
+			@Override
+			public void newMessage(Chat chat, User user, String message) {
+				mensajes.add(message);
+			}
+		};
 		chatManager.newUser(user);
 		chat.addUser(user);
-		semaforo.release();
-		latchUser.countDown();
-
+		nuevoUsuario.countDown();
+		
+		Thread.sleep(500);
+		for(String mensaje : mensajes){
+			System.out.println(mensaje);
+		}
+		boolean iguales = true;
+		for(int i = 0; i < 5 && iguales; i++){
+			iguales =(mensajes.get(i) == mensajesOrdenados[i]);
+		}
+		
+		intercambiador.exchange(iguales);
 	}
 }
-
