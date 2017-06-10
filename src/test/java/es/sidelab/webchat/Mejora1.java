854fc8a382c108a2629eb9e0a7c07d1327f750f7
cesarcuestavera <cesarcuestavera@gmail.com>
2017-06-10 11:16:34 +0200

    mejora1 arreglada

--- src/test/java/es/sidelab/webchat/Mejora1.java
+++ src/test/java/es/sidelab/webchat/Mejora1.java
@@ -1,14 +1,11 @@
 package es.sidelab.webchat;
 
 
-import java.util.ConcurrentModificationException;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletionService;
-import java.util.concurrent.ExecutorCompletionService;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Test;
 
@@ -17,98 +14,107 @@ import es.codeurjc.webchat.ChatManager;
 
 public class Mejora1 {
 	
-	ChatManager chats =  new ChatManager(50);//chat manager con un tope de 50 chats.
+	ChatManager chatManager = new ChatManager(50);
 	
-	ExecutorService executor1 = Executors.newSingleThreadExecutor();
-	CompletionService<String> cs1 = new ExecutorCompletionService<>(executor1);
+	ExecutorService executor = Executors.newSingleThreadExecutor();
 	ExecutorService executor2 = Executors.newSingleThreadExecutor();
-	CompletionService<String> cs2 = new ExecutorCompletionService<>(executor2);
 	ExecutorService executor3 = Executors.newSingleThreadExecutor();
-	CompletionService<String> cs3 = new ExecutorCompletionService<>(executor3);
 	ExecutorService executor4 = Executors.newSingleThreadExecutor();
-	CompletionService<String> cs4 = new ExecutorCompletionService<>(executor4);
 	
 	@Test
-	public void newUserInChat() throws ConcurrentModificationException, Throwable{
-		Callable<String> tarea1 = ()-> {
-			TestUser user = new TestUser("User0");
-			chats.newUser(user);
-			for(int i=0;i<5;i++){
-				Chat chat = chats.newChat("Chat "+i, 5,TimeUnit.SECONDS);
-				chat.addUser(user);
-			}
-			return null;
-		};
-		cs1.submit(tarea1);
-		Callable<String> tarea2 = ()-> {
-			TestUser user = new TestUser("User1");
-			chats.newUser(user);
-			for(int i=0;i<5;i++){
-				Chat chat = chats.newChat("Chat "+i, 5,TimeUnit.SECONDS);
-				chat.addUser(user);
-			}
-			return null;
-		};
-		cs2.submit(tarea2);
-		Callable<String> tarea3 = ()-> {
-			TestUser user = new TestUser("User2");
-			chats.newUser(user);
-			for(int i=0;i<5;i++){
-				Chat chat = chats.newChat("Chat "+i, 5,TimeUnit.SECONDS);
-				chat.addUser(user);
-			}
-			return null;
-		};
-		cs3.submit(tarea3);
-		Callable<String> tarea4 = ()-> {
-			TestUser user = new TestUser("User3");
-			chats.newUser(user);
-			for(int i=0;i<5;i++){
-				Chat chat = chats.newChat("Chat "+i, 5,TimeUnit.SECONDS);
-				chat.addUser(user);
-			}
-			return null;
-		};
-		cs4.submit(tarea4);
+	public void newUserInChat() throws Throwable {
 		
+		Runnable tarea = ()-> {
+			try {
+				mejoras("user0");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		};
+		executor.execute(tarea);
 		
-		try {
-			Future<String> f1= cs1.take();
+		Runnable tareaUser1 = ()-> {
+			
 			try {
-				System.out.println("Terminado "+f1.get());
-			} catch (ConcurrentModificationException e) {
+				mejoras("user1");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TimeoutException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			Future<String> f2 = cs2.take();
 			try {
-				System.out.println("Terminado "+f2.get());
-			} catch (ConcurrentModificationException e) {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			Future<String> f3 = cs3.take();
+		};
+		executor2.execute(tareaUser1);
+		
+		Runnable tareaUser2 = ()-> {
+			
 			try {
-				System.out.println("Terminado "+f3.get());
-			} catch (ConcurrentModificationException e) {
+				mejoras("user2");
+			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}	
-			Future<String> f4 = cs4.take();
-			try {
-				System.out.println("Terminado "+f4.get());
-			} catch (ConcurrentModificationException e) {
+			} catch (TimeoutException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} catch (ConcurrentModificationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		};
+		executor3.execute(tareaUser2);
+		
+		Runnable tareaUser3 = ()-> {
+			
+			try {
+				mejoras("user3");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (TimeoutException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		};
+		executor4.execute(tareaUser3);
+		
 	}
-
 	
-
-
-
+	public void mejoras (String stringhilo) throws InterruptedException, TimeoutException {
+		TestUser user = new TestUser(stringhilo);
+		chatManager.newUser(user);
+		for (int j = 0; j<5; j++ ){
+			Chat chat = chatManager.newChat("Chat"+j, 5, TimeUnit.SECONDS);
+			chat.addUser(user);
+			
+		}
+		
+		
+	}	
 }
