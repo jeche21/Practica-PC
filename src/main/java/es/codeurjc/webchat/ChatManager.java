35fbb30033bccdaacf9733b3888a017e4b5aff5c
jeche21 <jesus-alcorcon@hotmail.com>
2017-06-16 01:00:36 +0200

    Mejora7

--- src/main/java/es/codeurjc/webchat/ChatManager.java
+++ src/main/java/es/codeurjc/webchat/ChatManager.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
@@ -24,14 +25,77 @@ public class ChatManager {
 	un usuario con mala conexion haga que los demas tambien vayan lentos. De esta manera si tienes buena conexion no habra problema.*/
 	private ConcurrentHashMap<User,ExecutorService> tareas= new ConcurrentHashMap<>();
 	Semaphore semaforo = new Semaphore(1);
+	Executor numChats = (Executor) Executors.newScheduledThreadPool(1).scheduleAtFixedRate(null, 3, 3, TimeUnit.SECONDS);
+	Executor numUser = Executors.newSingleThreadExecutor();
+	Executor numMedUsuer =  Executors.newSingleThreadExecutor();
+	Executor chatMaxUser = Executors.newSingleThreadExecutor();
+	Executor chatMinUser = Executors.newSingleThreadExecutor();
+	
 
 	private int maxChats;
 	//Mejora_func1
 	private BlockingQueue<String> colaChats;//hacemos una blocking queue de 50 chat como maximo.
+	
 
 	public ChatManager(int maxChats) {
 		this.maxChats = maxChats;
 		this.colaChats = new ArrayBlockingQueue<>(maxChats);
+	}
+	
+	public void  estadisticas(){
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(true){
+			Runnable numeroChats = () ->{
+				System.out.println("El numero de chats es: " + chats.size());
+			};
+			numChats.execute(numeroChats);
+			Runnable numeroUsuarios = () -> {
+				int usuariosTotalesEnChats = this.numeroUsuariosTotales();
+				System.out.println("El numero de usuarios activos son: " + usuariosTotalesEnChats);
+			};
+			numUser.execute(numeroUsuarios);
+			Runnable mediaUser = () -> {
+				float mediaUsuariosChats = this.numeroUsuariosTotales() / (chats.size());
+				System.out.println("La media de usuarios por chat es de: " + mediaUsuariosChats);
+			};
+			numMedUsuer.execute(mediaUser);
+			Runnable maximoUserChats = () -> {
+				int max = 0;
+				Chat chatMayor = null;
+				for(Chat chat : this.getChats()){
+					if(chat.getUsers().size() >= max){
+						max = chat.getUsers().size();
+						chatMayor = chat;
+					}
+				}
+				System.out.println("El chat que mas usuarios tiene es: " + chatMayor.getName());
+			};
+			chatMaxUser.execute(maximoUserChats);
+			Runnable minimoUserChats = () -> {
+				int min = 0;
+				Chat chatMenor = null;
+				for(Chat chat : this.getChats()){
+					if(chat.getUsers().size() <= min){
+						min = chat.getUsers().size();
+						chatMenor = chat;
+					}
+				}
+				System.out.println("El chat que menos usuarios tiene es: " + chatMenor.getName());
+			};
+			chatMinUser.execute(minimoUserChats);
+			
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public void newUser(User user) {
@@ -46,8 +110,7 @@ public class ChatManager {
 			//mejora3 y mejora1
 			if(users.putIfAbsent(user.getName(), user) == null){
 				tareas.putIfAbsent(user, Executors.newSingleThreadExecutor());
-			}
-			
+			}		
 		}
 	}
 
@@ -121,4 +184,12 @@ public class ChatManager {
 	}
 
 	public void close() {}
+	
+	public int numeroUsuariosTotales(){
+		int contadorUser = 0;
+		for(Chat chat :this.getChats()){
+			contadorUser = contadorUser + chat.getUsers().size();
+		}
+		return contadorUser;
+	}
 }
